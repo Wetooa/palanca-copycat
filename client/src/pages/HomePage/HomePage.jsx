8f451a0532261f20278b3bcb55fb060733c0632d
Wetooa <103910243+Wetooa@users.noreply.github.com>
2022-08-30 20:04:07 +0800

    Improved homepage UI

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -14,6 +14,7 @@ import { Container } from "@mui/system";
 
 function HomePage() {
   const [filteredStudents, setFilteredStudents] = useState(allStudents);
+  const [section, setSection] = useState("all");
 
   return (
     <Box
@@ -27,35 +28,55 @@ function HomePage() {
       <Typography sx={{ textAlign: "end" }}>Sign In via Google</Typography>
       <Container
         sx={{
+          marginTop: "20px",
+          marginBottom: "10px",
           textAlign: "center",
           maxWidth: "770px",
           display: "flex",
           flexDirection: "column",
+          rowGap: "15px",
+          justifyContent: "center",
         }}
       >
-        <Typography variant="h2">Dear kōhai,</Typography>
-        <Typography>
+        <Typography variant="h2" sx={{ fontWeight: "bold" }}>
+          Dear kōhai,
+        </Typography>
+        <Typography variant="p">
           As our journey in Pisay comes to an end, new frontiers await us in the
           next chapter of our lives. Feel free to send us messages of support
           for us to take in our future endeavors.
         </Typography>
-        <Typography>“Lilipad at lalaban, gagawa ng kasaysayan”</Typography>
-        <Typography>-Gayang Mingor</Typography>
+        <Typography variant="p">
+          “Lilipad at lalaban, gagawa ng kasaysayan”
+        </Typography>
+        <Typography variant="p">-Gayang Mingor</Typography>
       </Container>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <TextField type="text" />
-        <Select label="Section">
-          <MenuItem>Block A</MenuItem>
-          <MenuItem>Block B</MenuItem>
-          <MenuItem>Block C</MenuItem>
+      <Box
+        sx={{
+          display: "flex",
+          margin: "20px 20px",
+          justifyContent: "center",
+        }}
+      >
+        <TextField
+          type="text"
+          sx={{ flexGrow: 2 }}
+          placeholder="sige kinsa man..."
+        />
+        <Select
+          label="Section"
+          sx={{ flexGrow: 1 }}
+          value={section}
+          onChange={(e) => setSection(e.target.value)}
+        >
+          <MenuItem value="All">All Section</MenuItem>
+          <MenuItem value="A">Block A</MenuItem>
+          <MenuItem value="B">Block B</MenuItem>
+          <MenuItem value="C">Block C</MenuItem>
         </Select>
       </Box>
       <Box sx={{ paddingTop: 10, alignContent: "center" }}>
-        <Grid
-          container
-          columns={{ sm: 2, md: 3, lg: 4 }}
-          spacing={{ xs: 1, sm: 2, md: 3 }}
-        >
+        <Grid container justifyContent={"space-around"}>
           {filteredStudents.map((student, index) => {
             return (
               <Grid item>
--- client/src/pages/HomePage/components/StudentFrame.jsx
+++ client/src/pages/HomePage/components/StudentFrame.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Avatar, Box, Button, Typography } from "@mui/material";
 
 function StudentFrame(student) {
   const { value, label, section, nickname } = student;
   return (
     <Box sx={{ width: 200, height: 250 }}>
-      <Typography>{value}</Typography>
-      <Button variant="contained">Send message</Button>
+      <img src="url(../../../images/batch/Sajulga.jpg)" alt={value} />
+      <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+        {value}
+      </Typography>
+      <Typography variant="p" sx={{ fontSize: "0.75rem" }}>
+        {nickname.length < 30 ? nickname : `${nickname.slice(0, 30)}...`}
+      </Typography>
     </Box>
   );
 }
